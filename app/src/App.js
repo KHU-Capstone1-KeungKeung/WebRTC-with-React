21c44837deeed74cffc7302450578abfbcc07322
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2021-02-18 12:30:00 +0900

    Update Setting Web RTC

--- app/src/App.js
+++ app/src/App.js
@@ -1,24 +1,55 @@
+import React, { useEffect, useRef, useState } from "react";
 import logo from './logo.svg';
 import './App.css';
 
+// RTCPeerConnection config
+const pc_config = {
+    iceServers: [
+        // {
+        //   urls: 'stun:[STUN_IP]:[PORT]',
+        //   'credentials': '[YOR CREDENTIALS]',
+        //   'username': '[USERNAME]'
+        // },
+        {
+            urls: "stun:stun.l.google.com:19302",
+        },
+    ],
+};
+const SOCKET_SERVER_URL = "http://localhost:8080";
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const videoState = useRef(true);
+    const audioState = useRef(true);
+    const pcRef = useRef<RTCPeerConnection>(null);
+    const localVideoRef = useRef<HTMLVideoElement>(null);
+    const remoteVideoRef = useRef<HTMLVideoElement>(null);
+
+
+    return (
+        <div>
+            <video
+                style={{
+                    width: 240,
+                    height: 240,
+                    margin: 5,
+                    backgroundColor: "black",
+                }}
+                muted
+                ref={localVideoRef}
+                autoPlay
+            />
+            <video
+                id="remotevideo"
+                style={{
+                    width: 240,
+                    height: 240,
+                    margin: 5,
+                    backgroundColor: "black",
+                }}
+                ref={remoteVideoRef}
+                autoPlay
+            />
+        </div>
   );
 }
 
