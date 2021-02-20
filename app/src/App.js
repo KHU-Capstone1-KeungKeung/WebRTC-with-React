cab077ae33d9202654762954334e6f0136727ed3
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2021-02-20 14:30:00 +0900

    Update web RTC create offer

--- app/src/App.js
+++ app/src/App.js
@@ -32,6 +32,33 @@ function App() {
         // webRTC 커넥션 생성
         pcRef.current = new RTCPeerConnection(pc_config);
 
+        const createOffer = async () => {
+            console.log("create offer");
+            if (!(pcRef.current && socketRef.current)) return;
+            try {
+                // 자신의 SessionDescription 이 담긴 offer 생성
+                const sdp = await pcRef.current.createOffer({
+                    offerToReceiveAudio: videoState.current,
+                    offerToReceiveVideo: audioState.current,
+                });
+
+                // 자신의 SessionDescription 저장
+                await pcRef.current.setLocalDescription(new RTCSessionDescription(sdp));
+
+                // 자신의 SessionDescription 을 담아 offer 이벤트 발생
+                socketRef.current.emit("offer", sdp);
+            } catch (e) {
+                console.error(e);
+            }
+        };
+
+        // offer 생성
+        socketRef.current.on("createOffer", (allUsers) => {
+            if (allUsers.length > 0) {
+                createOffer();
+            }
+        });
+
         return () => {
             if (socketRef.current) {
                 socketRef.current.disconnect();
